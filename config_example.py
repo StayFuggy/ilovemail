2976e6490769297d9b4be3e3161d58beec86f359
gdjohn4s <john4s@protonmail.ch>
2021-01-02 05:38:58 +0100

    added example config file

---
+++ config_example.py
@@ -0,0 +1,20 @@
+# - Configuration file to manage mail configuration
+
+# - Script info
+__version__ = '0.0.1'
+__author__ = 'Giovanni'
+
+# - Login and Send params
+mail_from = 'sender email'
+mail_to = 'receiver email'
+password = 'your password'
+
+# - Server connection params
+# Google SMTP
+smtp_server = 'smtp.gmail.com'
+port = 465
+TLSport = 587
+
+# Outlook SMTP
+
+# Libero SMTP
