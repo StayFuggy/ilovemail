ef7ff54b158a9284c4c4edd8bc0ba8a5785b09ff
gdjohn4s <john4s@protonmail.ch>
2021-01-04 03:42:30 +0100

    updated

--- test_mail.py
+++ test_mail.py
@@ -3,4 +3,16 @@ from smtplib import SMTP
 # - Cast response message in list to identify HTTP code
 with SMTP('smtp.google.com') as smtp:
     messg = list(smtp.noop())
-    print(messg[0])
+    print(messg)
+
+
+with SMTP('smtp.office365.com') as smtp:
+    messg = list(smtp.noop())
+    print(messg)
+
+
+with SMTP('smtp.libero.it') as smtp:
+    messg = list(smtp.noop())
+    print(messg)
+
+
