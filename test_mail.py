df6107cf82b2e6f78f696edb178c62a0c9051707
gdjohn4s <john4s@protonmail.ch>
2021-01-07 01:27:54 +0100

    test

--- test_mail.py
+++ test_mail.py
@@ -1,18 +1,26 @@
 from smtplib import SMTP
+from time import sleep
+from rich.console import Console
 
-# - Cast response message in list to identify HTTP code
-with SMTP('smtp.google.com') as smtp:
-    messg = list(smtp.noop())
-    print(messg)
+console = Console()
+tasks = [f"task"]
 
+with console.status("[bold green]Working on tasks...") as status:
+    while tasks:
+        tasks = tasks.pop(0)
+        sleep(1)
+        # - Cast response message in list to identify HTTP code
+        with SMTP('smtp.google.com') as smtp:
+            messg = list(smtp.noop())
+            print(messg)
 
-with SMTP('smtp.office365.com') as smtp:
-    messg = list(smtp.noop())
-    print(messg)
+        with SMTP('smtp.office365.com') as smtp:
+            messg = list(smtp.noop())
+            print(messg)
 
+        with SMTP('smtp.libero.it') as smtp:
+            messg = list(smtp.noop())
+            print(messg)
 
-with SMTP('smtp.libero.it') as smtp:
-    messg = list(smtp.noop())
-    print(messg)
-
+        console.log(f"{task} complete")
 
