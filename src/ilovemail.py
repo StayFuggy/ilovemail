c23af94d28f03d42f78e8310759be6712695fe38
gdjohn4s <john4s@protonmail.ch>
2021-12-06 23:36:58 +0100

    check if SSL is True, if HTML Message is True, clean some code

--- src/ilovemail.py
+++ src/ilovemail.py
@@ -8,7 +8,7 @@ from email.mime.text import MIMEText
 """
 Name: ilovemail
 Author: gdjohn4s
-Version: v2.1.1
+Version: v2.1.4
 """
 
 # Setting default logging properties
@@ -57,6 +57,7 @@ def check_smtp(server: str) -> bool:
         messg: list = list(smtp.noop())
         messg: str = str(messg[0])
         if messg == '250' or '200' or '201' or '202':
+            logging.info(f'Server {SMTP_SERVER} OK {messg} - Checking SSL Encryption')
             return True
         else:
             logging.error(f'Error connecting to {server}')
@@ -70,21 +71,20 @@ def get_context() -> ssl.SSLContext:
     return ssl.create_default_context()
 
 
-def send_mail(
-    SMTP_SERVER: str,
-    SMTP_PORT: str,
-    MAIL_FROM: str,
-    MAIL_PWD: str,
-    MAIL_TO: str,
-    ) -> bool:
+def send_mail() -> bool:
     """
     Check if SSL is enabled in config file, connect to the SMTP server
     and using basic auth send the email defined in config.py
     """
-    if SSL_ENABLE == True:
+    if SSL_ENABLE:
         logging.info("Connecting with SSL Enabled")
         try:
-            msg: MIMEText = MIMEText(get_html(BODY_FILE_HTML), 'html')
+            if HTML_ENABLE:
+                logging.info("HTML Mime Enabled, getting the body")
+                msg: MIMEText = MIMEText(get_html(BODY_FILE_HTML), 'html')
+            else:
+                logging.info("HTML Mime Disabled, using normal message as body")
+                msg: MIMEText = MIMEText(BODY)
             msg['Subject'] = SUBJECT
             msg['From'] = MAIL_FROM
             msg['To'] = MAIL_TO
@@ -96,10 +96,12 @@ def send_mail(
                 logging.info("Email Sent!")
                 mail.close()
         except Exception as e:
-            logging.error(f'Error sending email {e}')
+            logging.error(f'Email not sent {e}')
         finally:
             mail.close()
-
+    else:
+        logging.warning("SSL Connection disabled, for your security, the script will terimnated!")
+        exit(1)
 
 def main() -> int:
     """
@@ -108,16 +110,9 @@ def main() -> int:
     context: ssl.SSLContext = get_context()
 
     if check_smtp(SMTP_SERVER):
-        logging.info(f'Server {SMTP_SERVER} OK - Sending the Email')
-        send_mail(
-            SMTP_SERVER=SMTP_SERVER,
-            SMTP_PORT=SMTP_PORT,
-            MAIL_FROM=MAIL_FROM,
-            MAIL_PWD=MAIL_PWD,
-            MAIL_TO=MAIL_TO,
-            )
+        send_mail()
     else:
-        logging.error("Server {SMTP_SERVER} KO - not responding")
+        logging.error(f'Server {SMTP_SERVER} KO - not responding')
 
 
 if __name__ == "__main__":
