4de1d01b9dff08dac2d9b4c744a8e301511af3d6
gdjohn4s <john4s@protonmail.ch>
2021-12-06 23:36:13 +0100

    added CC and HTML enable/disable based on config

--- src/config_sample.py
+++ src/config_sample.py
@@ -1,20 +1,23 @@
 # General Configuration
 __author__ = 'gdjohn4s'
-LOG: str = 'ilovemail.log'
+from src.config import HTML_ENABLE
+
+
+LOG: str = r'ilovemail.log'
 
 # SMTP Server Auth
-MAIL_FROM: str = "mail@from.com"
-MAIL_PWD: str = "password"
-MAIL_TO: str = "mailto@one.com", "mailto@two.com"
-MAIL_CC: str = ""
-MAIL_BCC: str = ""
+MAIL_FROM: str = 'mail@from.com'
+MAIL_PWD: str = 'password'
+MAIL_TO: str = 'mailto@one.com', 'mailto@two.com'
+MAIL_CC: str = ''
 
 # SMTP Options
-SMTP_SERVER: str = "smtp-mail.outlook.com"
+SMTP_SERVER: str = 'smtp-mail.outlook.com'
 SMTP_PORT: int = 587
 SSL_ENABLE: bool = True
 
 # Mail Options
-SUBJECT: str = "HELLO"
-BODY: str = "My friend" # -> You can set html here like """ <html> """
-BODY_FILE_HTML: str = "body.html"
+SUBJECT: str = 'HELLO'
+HTML_ENABLE: bool = True
+BODY: str = 'My friend' # -> You can set html here like ''' <html> '''
+BODY_FILE_HTML: str = r'body.html'
